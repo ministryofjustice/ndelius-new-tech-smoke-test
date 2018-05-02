2441805a4ee2795053fc42fdeb20a8d08884da7f
Andy Marke <andy.marke@digital.justice.gov.uk>
2018-05-02 16:03:13 +0100

    116 filter by provider (#15)
    * 116 FilterByProvider

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/Config.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/Config.groovy
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.delius
 
 class Config {
     static elasticSearchBaseUrl() {
-        System.getenv('ELASTIC_SEARCH_URL') ?: "https://search-offender-smoke-test-766apkmmedr4zssxm3og4ssohq.eu-west-2.es.amazonaws.com/"
+        System.getenv('ELASTIC_SEARCH_URL') ?: "http://localhost:9200/"
     }
     static mongoConnectionUrl() {
         System.getenv('ANALYTICS_MONGO_CONNECTION') ?: 'mongodb://localhost'
