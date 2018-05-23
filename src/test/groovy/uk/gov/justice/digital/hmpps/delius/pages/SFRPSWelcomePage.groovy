6ad62074b75dafbe04c5f9b93c176431e93da82d
Andy Marke <andy.marke@gmail.com>
2018-05-23 14:59:21 +0100

    306 Footer with buttons

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/SFRPSWelcomePage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/SFRPSWelcomePage.groovy
@@ -8,8 +8,8 @@ class SFRPSWelcomePage extends Page {
 
     static content = {
         heading { $("h1").text() }
-        startNowButton { $("input[value='Start now']") }
-        continueNowButton { $("input[value='Continue now']") }
+        startNowButton { $("button", text: "Start now") }
+        continueNowButton { $("button", text: "Continue now") }
         feedbackLink {$("a", text: "feedback")}
         documentListLink {$("a", text: "Back to document list")}
     }
