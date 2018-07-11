6813b1d9e58978c21432dceaf8af827e8dd6d24c
Andy Marke <andy.marke@gmail.com>
2018-07-11 11:49:03 +0100

    [AM] 510 Parole report landing page - try restarting browser to fix old sfpsr appearing in popup

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1PrisonerDetailsPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1PrisonerDetailsPage.groovy
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.delius.pages
 
 class PP1PrisonerDetailsPage extends SFPSRBasePage {
 
-    static at = { heading == "Prisoner details" }
+    static at = { $("h1").text() == "Prisoner details" }
 
     static content = {
         heading { $("h1").text() }
