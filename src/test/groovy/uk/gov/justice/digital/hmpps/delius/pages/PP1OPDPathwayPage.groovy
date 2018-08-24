420d92b272334d1e11b7be96c5ecd159ac3e983a
Andy Marke <andy.marke@digital.justice.gov.uk>
2018-08-24 15:21:55 +0100

    [AM] 66 OPD Pathway (#34)

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1OPDPathwayPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1OPDPathwayPage.groovy
@@ -4,4 +4,8 @@ class PP1OPDPathwayPage extends PP1BasePage {
 
     static at = { heading == "OPD pathway" }
 
+    static content = {
+        setOPDPathwayServicesYes { $("#consideredForOPDPathwayServices_yes").value("yes") }
+    }
+
 }
