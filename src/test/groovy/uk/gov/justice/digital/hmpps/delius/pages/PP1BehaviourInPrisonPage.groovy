7d8bb584188849c401ae138d6a5b9b988112d213
Paul Massey <paul.massey@scriptwerx.io>
2018-08-28 14:18:15 +0100

    Updated tests

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1BehaviourInPrisonPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1BehaviourInPrisonPage.groovy
@@ -3,4 +3,9 @@ package uk.gov.justice.digital.hmpps.delius.pages
 class PP1BehaviourInPrisonPage extends PP1BasePage {
 
     static at = { heading == "Behaviour in prison" }
+
+    static content = {
+        fillBehaviourDetailWith { text -> $("#behaviourDetail .ql-editor").leftShift(text) }
+        fillRotlSummaryWith { text -> $("#rotlSummary .ql-editor").leftShift(text) }
+    }
 }
