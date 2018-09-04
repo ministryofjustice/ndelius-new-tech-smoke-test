b4feeecb46c4ae5fb98f1c8a3d0f0fbeb9dfe2d8
Paul Massey <paul.massey@scriptwerx.io>
2018-09-04 09:05:20 +0100

    Updated tests

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RoshAnalysisPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RoshAnalysisPage.groovy
@@ -2,4 +2,13 @@ package uk.gov.justice.digital.hmpps.delius.pages
 
 class PP1RoshAnalysisPage extends PP1BasePage {
     static at = { heading == "RoSH analysis" }
+
+    static content = {
+        fillNatureOfRisk { text -> $("#natureOfRisk .ql-editor").leftShift(text) }
+        fillIncreaseFactors { text -> $("#increaseFactors .ql-editor").leftShift(text) }
+        fillDecreaseFactors { text -> $("#decreaseFactors .ql-editor").leftShift(text) }
+        fillLikelihoodFurtherOffending { text -> $("#likelihoodFurtherOffending .ql-editor").leftShift(text) }
+        setRiskOfAbscondingYes { $("#riskOfAbsconding_yes").value("yes") }
+        fillRiskOfAbscondingDetails { text -> $("#riskOfAbscondingDetails .ql-editor").leftShift(text) }
+    }
 }
