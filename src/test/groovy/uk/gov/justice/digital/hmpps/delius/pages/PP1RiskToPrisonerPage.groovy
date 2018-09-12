56e9960d3f37bc9c7dc0a715fd35e22759b93660
Paul Massey <theDustRoom@users.noreply.github.com>
2018-09-12 16:19:47 +0100

    96 risk to prisoner (#48)
    * [PM] - Included risk to prisoners page

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskToPrisonerPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskToPrisonerPage.groovy
@@ -2,4 +2,11 @@ package uk.gov.justice.digital.hmpps.delius.pages
 
 class PP1RiskToPrisonerPage extends PP1BasePage {
     static at = { heading == "Risk to the prisoner" }
+
+    static content = {
+        setSelfHarmCommunityYes { $("#selfHarmCommunity_yes").value("yes") }
+        setSelfHarmCustodyYes { $("#selfHarmCustody_yes").value("yes") }
+        setOthersHarmCommunityYes { $("#othersHarmCommunity_yes").value("yes") }
+        setOthersHarmCustodyYes { $("#othersHarmCustody_yes").value("yes") }
+    }
 }
