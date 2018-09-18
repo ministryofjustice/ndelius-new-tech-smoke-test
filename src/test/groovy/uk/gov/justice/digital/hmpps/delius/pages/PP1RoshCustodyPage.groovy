376a39c34e37e56a2ff5e7cb419f115938347744
Paul Massey <paul.massey@scriptwerx.io>
2018-09-18 08:56:55 +0100

    [PM] - Included Current RoSH custody page

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RoshCustodyPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RoshCustodyPage.groovy
@@ -1,5 +1,13 @@
 package uk.gov.justice.digital.hmpps.delius.pages
 
 class PP1RoshCustodyPage extends PP1BasePage {
-    static at = { heading == "Current ROSH: custody" }
+    static at = { heading == "Current RoSH: custody" }
+
+    static content = {
+        setRoshCustodyPublicLow { $("#roshCustodyPublic_low").value("low") }
+        setRoshCustodyKnownAdultMedium { $("#roshCustodyKnownAdult_medium").value("medium") }
+        setRoshCustodyChildrenHigh { $("#roshCustodyChildren_high").value("high") }
+        setRoshCustodyPrisonersVeryHigh { $("#roshCustodyPrisoners_very_high").value("very_high") }
+        setRoshCustodyStaffLow { $("#roshCustodyStaff_low").value("low") }
+    }
 }
