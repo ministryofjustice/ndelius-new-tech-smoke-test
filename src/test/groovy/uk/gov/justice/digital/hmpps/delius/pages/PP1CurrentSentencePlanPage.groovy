3ef5756eba0ca169b8708c9ccfca949e7bbd1bda
Paul Massey <theDustRoom@users.noreply.github.com>
2018-11-27 14:19:47 +0000

    Updated copy (#83)

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1CurrentSentencePlanPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1CurrentSentencePlanPage.groovy
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.delius.pages
 
 class PP1CurrentSentencePlanPage extends PP1BasePage {
 
-    static at = { heading == "Current sentence plan and response" }
+    static at = { heading == "Prison sentence plan and response" }
 
     static content = {
         fillSentencePlanWith { text -> $("#sentencePlan .ql-editor").leftShift(text) }
