33ad689c242ea103764e7d6dc6e048a4c6418d4e
Paul Massey <paul.massey@scriptwerx.io>
2018-11-27 11:36:14 +0000

    Updated to include support under RMP

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskManagementPlanPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskManagementPlanPage.groovy
@@ -7,6 +7,7 @@ class PP1RiskManagementPlanPage extends PP1BasePage {
         setRiskManagementPlanRequiredYes { $("#riskManagementPlanRequired_yes").value("yes") }
         fillCurrentSituation { text -> $("#currentSituation .ql-editor").leftShift(text) }
         fillSupportingAgencies { text -> $("#supportingAgencies .ql-editor").leftShift(text) }
+        fillSupport { text -> $("#support .ql-editor").leftShift(text) }
         fillControl { text -> $("#control .ql-editor").leftShift(text) }
         fillRiskMeasures { text -> $("#riskMeasures .ql-editor").leftShift(text) }
         fillAgencyActions { text -> $("#agencyActions .ql-editor").leftShift(text) }
