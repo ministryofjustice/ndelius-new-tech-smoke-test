c133a689226a5995d167e0ed0624df40d129196c
Paul Massey <paul.massey@scriptwerx.io>
2019-04-24 10:08:11 +0100

    Added if around headless flag

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -5,7 +5,9 @@ driver = {
     def chromeDriverPath = getClass().getResource(String.format("webdriver/%s/chromedriver", isMac() ? "mac64" : "linux64"))
     System.setProperty('webdriver.chrome.driver', chromeDriverPath.getPath())
     def options = new ChromeOptions();
-    options.addArguments("--headless");
+    if (!isMac()) {
+        options.addArguments("--headless");
+    }
     options.addArguments("window-size=1920,1080");
     options.addArguments("--disable-gpu");
 
@@ -18,7 +20,6 @@ static def isMac() {
     def OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
     return ((OS.contains("mac")) || (OS.contains("darwin")));
 }
-
 
 baseUrl = System.getenv('DELIUS_WRAPPER_URL') ?: "http://localhost:3000/"
 reportsDir = "build/geb-reports"
