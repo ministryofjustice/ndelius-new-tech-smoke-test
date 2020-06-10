e3cda0cf398541eae4d58046e9a8b26d18cff774
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-06-10 16:46:07 +0100

    Updated methods
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskManagementPlanPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/delius/pages/PP1RiskManagementPlanPage.groovy
@@ -6,13 +6,10 @@ class PP1RiskManagementPlanPage extends PP1BasePage {
     static content = {
         setRiskManagementPlanRequiredYes { $("#riskManagementPlanRequired_yes").value("yes") }
         fillCurrentSituation { text -> js.exec("tinymce.get('currentSituation-tinymce').setContent('$text'); tinymce.get('currentSituation-tinymce').fire('blur'); return true") }
-        fillSupportingAgencies { text -> js.exec("tinymce.get('supportingAgencies-tinymce').setContent('$text'); tinymce.get('supportingAgencies-tinymce').fire('blur'); return true") }
-        fillSupport { text -> js.exec("tinymce.get('support-tinymce').setContent('$text'); tinymce.get('support-tinymce').fire('blur'); return true") }
-        fillControl { text -> js.exec("tinymce.get('control-tinymce').setContent('$text'); tinymce.get('control-tinymce').fire('blur'); return true") }
-        fillRiskMeasures { text -> js.exec("tinymce.get('riskMeasures-tinymce').setContent('$text'); tinymce.get('riskMeasures-tinymce').fire('blur'); return true") }
-        fillAgencyActions { text -> js.exec("tinymce.get('agencyActions-tinymce').setContent('$text'); tinymce.get('agencyActions-tinymce').fire('blur'); return true") }
-        fillAdditionalConditions { text -> js.exec("tinymce.get('additionalConditions-tinymce').setContent('$text'); tinymce.get('additionalConditions-tinymce').fire('blur'); return true") }
-        fillLevelOfContact { text -> js.exec("tinymce.get('levelOfContact-tinymce').setContent('$text'); tinymce.get('levelOfContact-tinymce').fire('blur'); return true") }
+        fillSupervision { text -> js.exec("tinymce.get('supervision-tinymce').setContent('$text'); tinymce.get('supervision-tinymce').fire('blur'); return true") }
+        fillMonitoringControl { text -> js.exec("tinymce.get('monitoringControl-tinymce').setContent('$text'); tinymce.get('monitoringControl-tinymce').fire('blur'); return true") }
+        fillInterventionsTreatment { text -> js.exec("tinymce.get('interventionsTreatment-tinymce').setContent('$text'); tinymce.get('interventionsTreatment-tinymce').fire('blur'); return true") }
+        fillVictimSafetyPlanning { text -> js.exec("tinymce.get('victimSafetyPlanning-tinymce').setContent('$text'); tinymce.get('victimSafetyPlanning-tinymce').fire('blur'); return true") }
         fillContingencyPlan { text -> js.exec("tinymce.get('contingencyPlan-tinymce').setContent('$text'); tinymce.get('contingencyPlan-tinymce').fire('blur'); return true") }
     }
 }
